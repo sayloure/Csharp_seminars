7d57187af7e2fa9a77fbe1fc4671c313369bfc1d
sayloure <135940846+sayloure@users.noreply.github.com>
2023-06-23 16:40:03 +0300

    init git and add .gitignore

---
+++ task02_дни_недели/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Runtime.CompilerServices;
+/*
+Напишите программу, которая будет выдавать название дня недели по заданному номеру.
+3 -> Среда
+5 -> Пятница
+*/
+
+int userInput = Convert.ToInt32(Console.ReadLine());
+if(userInput == 1)
+Console.WriteLine("Понедельник");
+if(userInput == 2)
+Console.WriteLine("Вторник");
+if(userInput == 3)
+Console.WriteLine("Среда");
+if(userInput == 4)
+Console.WriteLine("Четверг");
+if(userInput == 5)
+Console.WriteLine("Пятница");
+if(userInput == 6)
+Console.WriteLine("Суббота");
+if(userInput == 7)
+Console.WriteLine("Воскресенье");
