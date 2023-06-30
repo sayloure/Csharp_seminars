98498fd182e27cd237c4e8eaa7372e80cd4543b1
sayloure <135940846+sayloure@users.noreply.github.com>
2023-06-30 23:40:17 +0300

    Дз семинар 2

---
+++ task10_dz/Program.cs
@@ -0,0 +1,12 @@
+﻿/*
+Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+
+Console.Write("Введите трехзначное число - ");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+
+int numberTwo = (userNumber % 100) / 10;
+Console.Write(numberTwo);
---
+++ task13_dz/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6
+*/
+
+Console.Write("Введите число - ");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+
+string numberThree = userNumber.ToString();
+if (numberThree.Length > 2)
+{
+    Console.Write(numberThree[2]);
+}
+else
+{
+    Console.Write("Третьей цифры нет");
+}
