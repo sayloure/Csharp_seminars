c30a2ea3156a49a1edf1029289f1c58b7364f005
sayloure <135940846+sayloure@users.noreply.github.com>
2023-07-20 14:19:49 +0300

    Семинар 9

---
+++ task63/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+Задайте значение N. Напишите программу, которая выведет все натуральные числа 
+в промежутке от 1 до N.
+N = 5 -> "1, 2, 3, 4, 5"
+N = 6 -> "1, 2, 3, 4, 5, 6"
+*/
+
+void NaturalNumbers(int userN, int count = 1)
+{
+    if (userN < count)
+    {
+        return;
+    }
+    else
+    {
+        Console.Write(count + " ");
+        NaturalNumbers(userN, count + 1);
+    }
+}
+
+int n = 5;
+NaturalNumbers(n);
---
+++ task67/Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+453 -> 12
+45 -> 9
+*/
+
+int RecSum(int num)
+{
+    if(num != 0)
+    {
+        return num % 10 + RecSum(num / 10);
+    }
+    return 0;
+}
+
+Console.WriteLine(RecSum(453));
+
+/* Вариант 2
+int SumOfDigits(int number)
+{
+    if (number < 10)
+    {
+        return number;
+    }
+    else
+    {
+        return number % 10 + SumOfDigits(number / 10);
+    }
+}
+*/
